83d0485f44d1f64fbe3dfd11d8b63b955baf1e09
Heo-Tae-Min <71204131+Heo-Tae-Min@users.noreply.github.com>
2023-02-22 09:45:37 +0900

    [Silver III] Title: N과 M (4), Time: 152 ms, Memory: 16064 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[] selected;
+	static StringBuilder sb;
+	
+	static void select(int cnt, int start) {
+		if(cnt == M) {
+			for(int num : selected) {
+				sb.append(num).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=start; i<=N; i++) {
+			selected[cnt] = i;
+			select(cnt+1, i);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		sb = new StringBuilder();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		selected = new int[M];
+		
+		select(0, 1);
+		
+		bw.write(sb.toString());
+		bw.flush(); bw.close(); br.close();
+	}
+
+}
